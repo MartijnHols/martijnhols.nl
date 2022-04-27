fa70257595218866208da5b0a1ac947e5d629fe3
Martijn Hols <git@martijnhols.nl>
2022-04-27 16:36:03 +0200

    Increase base font sizes

--- theme/fontSizes.ts
+++ theme/fontSizes.ts
@@ -1,5 +1,5 @@
-export const smallestText = 10 // TODO rename
-export const tertiaryText = 12
-export const secondaryText = 14
-export const mainText = 15
-export const input = 16
+export const smallestText = 12 // TODO rename
+export const tertiaryText = 14
+export const secondaryText = 16
+export const mainText = 18
+export const input = 18
