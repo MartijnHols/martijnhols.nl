b051f8c73a5ddeabe27d161d47ad6dc05d194762
Martijn Hols <git@martijnhols.nl>
2022-04-27 21:27:25 +0200

    Flesh out ProjectsSlice

--- slices/ProjectsSlice/index.tsx
+++ slices/ProjectsSlice/index.tsx
@@ -1,15 +1,50 @@
 import styled from "@emotion/styled";
-import { ImageField, LinkField, Slice, TitleField } from "@prismicio/types";
+import {
+  ImageField,
+  KeyTextField,
+  LinkField,
+  RichTextField,
+  Slice,
+  TitleField,
+} from "@prismicio/types";
 import Image from "next/image";
 
 import Container from "../../components/Container";
+import PrismicRichText from "../../components/PrismicRichText";
 import PrismicTitle from "../../components/PrismicTitle";
-import { colors } from "../../theme";
+import { colors, spacing } from "../../theme";
 import convertPrismicImage from "../../utils/convertPrismicImage";
 
 const Section = styled.div`
   background: ${colors.dominant};
+  color: ${colors.complementary};
   padding: 150px 0;
+`;
+const Project = styled.div`
+  display: flex;
+  gap: ${spacing.x6}px;
+  padding: ${spacing.x2}px 0;
+
+  :not(:first-of-type) {
+    border-top: 3px solid ${colors.complementary};
+  }
+`;
+const ProjectImage = styled.div`
+  flex: 0 0 auto;
+`;
+const Explanation = styled.div`
+  flex: 1 1 auto;
+`;
+const ProjectAbout = styled.div`
+  margin-bottom: ${spacing.x2}px;
+`;
+const TechItem = styled.div`
+  display: inline-block;
+  background: ${colors.complementary};
+  color: ${colors.dominant};
+  padding: 4px 6px;
+  margin-right: 2px;
+  margin-bottom: 2px;
 `;
 
 export type PrismicProjectsSlice = Slice<
@@ -20,6 +55,8 @@ export type PrismicProjectsSlice = Slice<
   {
     image: ImageField;
     url: LinkField;
+    about: RichTextField;
+    tech: KeyTextField;
   }
 >;
 
@@ -41,15 +78,28 @@ const ProjectsSlice = ({ slice }: Props) => (
         }
 
         return (
-          <span key={index}>
-            <Image
-              src={image.src}
-              alt={image.alt}
-              width={200}
-              height={200}
-              objectFit="contain"
-            />
-          </span>
+          <Project key={index}>
+            <ProjectImage>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={200}
+                height={200}
+                objectFit="contain"
+              />
+            </ProjectImage>
+            <Explanation>
+              <ProjectAbout>
+                <PrismicRichText field={project.about} />
+              </ProjectAbout>
+              {project.tech
+                ?.split(",")
+                .map((item) => item.trim())
+                .map((item) => (
+                  <TechItem key={item}>{item}</TechItem>
+                ))}
+            </Explanation>
+          </Project>
         );
       })}
     </Container>
