955abd1d7778015de426098e4bdbafc876594a02
Martijn Hols <git@martijnhols.nl>
2022-04-29 13:55:18 +0200

    Add PrismicProvider for proper rich text links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
+import { LinkProps, PrismicProvider } from "@prismicio/react";
 import { AppProps } from "next/app";
 
+import Link from "../components/Link";
 import GlobalStyles from "../theme/GlobalStyles";
+import prismicLinkResolver from "../utils/prismicLinkResolver";
+
+const PrismicLink = ({ children, ...others }: LinkProps) => (
+  <Link {...others}>{children}</Link>
+);
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <PrismicProvider
+    linkResolver={prismicLinkResolver}
+    internalLinkComponent={PrismicLink}
+    externalLinkComponent={PrismicLink}
+  >
     <GlobalStyles />
     <Component {...pageProps} />
-  </>
+  </PrismicProvider>
 );
 
 export default App;
