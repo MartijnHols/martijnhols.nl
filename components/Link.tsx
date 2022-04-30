2c56f097cbdb0d7a5da655bad4956a25b04b1dfe
Martijn Hols <git@martijnhols.nl>
2022-04-30 22:16:27 +0200

    Fix linking to /nl from /en site

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,5 +1,8 @@
+import getConfig from "next/config";
 import NextLink from "next/link";
 import { AnchorHTMLAttributes, ComponentProps, ReactNode } from "react";
+
+const { publicRuntimeConfig } = getConfig();
 
 /**
  * This component simplifies next/link by adding an anchor element always. We
@@ -26,19 +29,49 @@ const Link = ({
   prefetch,
   locale,
   ...others
-}: Props) => (
-  <NextLink
-    href={href}
-    as={as}
-    replace={replace}
-    scroll={scroll}
-    shallow={shallow}
-    prefetch={prefetch}
-    locale={locale}
-    passHref
-  >
-    <a {...others}>{children}</a>
-  </NextLink>
-);
+}: Props) => {
+  const isAbsolute = href.startsWith(publicRuntimeConfig.primaryHost);
+  // Make absolute URLs relative to benefit from preloading, and make the URLs
+  // work on any domain.
+  // This is necessary because in Prismic we can only enter absolute URLs.
+  if (isAbsolute) {
+    href = href.substring(publicRuntimeConfig.primaryHost.length);
+
+    // Automatically detect the locale from absolute URLs as Next otherwise will
+    // treat them as a regular page.
+    // This is necessary because in Prismic when generating a link we can't
+    // select documents from other locales (this only works for the Content
+    // Relationship field), so we must use an absolute URL.
+    const hrefLocale =
+      !locale &&
+      Object.values(
+        publicRuntimeConfig.prismicLocaleMap as {
+          [prismicLocale: string]: string;
+        }
+      ).find(
+        (userLocale) =>
+          href.startsWith(`/${userLocale}/`) || href === `/${userLocale}`
+      );
+    if (hrefLocale) {
+      href = href.substring(`/${hrefLocale}`.length);
+      locale = hrefLocale;
+    }
+  }
+
+  return (
+    <NextLink
+      href={href}
+      as={as}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      prefetch={prefetch}
+      locale={locale  }
+      passHref
+    >
+      <a {...others}>{children}</a>
+    </NextLink>
+  );
+};
 
 export default Link;
