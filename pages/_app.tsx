725488d3d0ff5eb7aedb1bdcccde70b4afb391e4
Martijn Hols <git@martijnhols.nl>
2022-05-04 16:58:44 +0200

    Use react-lazy-hydration to delay hydration until idle
    Improves LH TBT

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { DehydratedState } from "react-query";
+import LazyHydrate from "react-lazy-hydration";
 
 import PrismicProvider from "../components/PrismicProvider";
 import ReactQueryProvider from "../components/ReactQueryProvider";
@@ -10,12 +11,14 @@ type MyAppProps = AppProps<{
 }>;
 
 const App = ({ Component, pageProps }: MyAppProps) => (
-  <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
-    <PrismicProvider>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </PrismicProvider>
-  </ReactQueryProvider>
+  <LazyHydrate whenIdle>
+    <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
+      <PrismicProvider>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </PrismicProvider>
+    </ReactQueryProvider>
+  </LazyHydrate>
 );
 
 export default App;
