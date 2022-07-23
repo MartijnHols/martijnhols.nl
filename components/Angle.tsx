5d6bd7aa7ad4891ce2296eac7a75073f79610970
Martijn Hols <git@martijnhols.nl>
2022-07-23 14:26:02 +0200

    Refactor Angle: move background color into styled components

--- components/Angle.tsx
+++ components/Angle.tsx
@@ -13,18 +13,28 @@ export const clipPathBorderFix = css`
 const Container = styled.div`
   position: relative;
 `
-const TopAngle = styled.div`
-  position: absolute;
-  width: 100%;
-  height: calc(10px + 100vw / 2000 * 30);
-  clip-path: polygon(0 0, 100% 0, 0 100%);
-  ${clipPathBorderFix}
-`
-const BottomAngle = styled.div`
-  height: calc(10px + 100vw / 2000 * 30);
-  clip-path: polygon(100% 0, 100% 100%, 0 100%);
-  ${clipPathBorderFix}
-`
+const TopAngle = styled('div', {
+  shouldForwardProp: (prop) => prop !== 'inverted',
+})<{ inverted?: boolean }>(
+  ({ inverted }) => css`
+    position: absolute;
+    width: 100%;
+    height: calc(10px + 100vw / 2000 * 30);
+    clip-path: polygon(0 0, 100% 0, 0 100%);
+    background: ${inverted ? colors.dominant : colors.complementary};
+    ${clipPathBorderFix}
+  `,
+)
+const BottomAngle = styled('div', {
+  shouldForwardProp: (prop) => prop !== 'inverted',
+})<{ inverted?: boolean }>(
+  ({ inverted }) => css`
+    height: calc(10px + 100vw / 2000 * 30);
+    clip-path: polygon(100% 0, 100% 100%, 0 100%);
+    background: ${inverted ? colors.complementary : colors.dominant};
+    ${clipPathBorderFix}
+  `,
+)
 
 interface Props {
   inverted?: boolean
@@ -32,14 +42,10 @@ interface Props {
 
 const Angle = ({ inverted }: Props) => (
   <Container>
-    <TopAngle
-      style={{ background: inverted ? colors.dominant : colors.complementary }}
-    >
+    <TopAngle inverted={inverted}>
       <ContactButton inverted={!inverted} aria-hidden tabIndex={-1} />
     </TopAngle>
-    <BottomAngle
-      style={{ background: inverted ? colors.complementary : colors.dominant }}
-    >
+    <BottomAngle inverted={inverted}>
       <ContactButton inverted={inverted} aria-hidden tabIndex={-1} />
     </BottomAngle>
   </Container>
