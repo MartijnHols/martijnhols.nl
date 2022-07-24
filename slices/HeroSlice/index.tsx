1c05bf3497e669ced42f1660ba0701bf2a6b7b24
Martijn Hols <git@martijnhols.nl>
2022-07-24 20:11:59 +0200

    Store selected language in a cookie

--- slices/HeroSlice/index.tsx
+++ slices/HeroSlice/index.tsx
@@ -156,11 +156,20 @@ const HeroSlice = ({ slice }: Props) => {
   const { locale } = useRouter()
   const config = usePrismicConfig()
 
+  const alternativeLocale = locale === 'nl' ? 'en' : 'nl'
+  const handleSwitchLanguage = () => {
+    document.cookie = `NEXT_LOCALE=${alternativeLocale}; max-age=31536000; path=/`
+  }
+
   return (
     <Wrapper>
       <Header>
         <HeaderContent className="inverted">
-          <Link href="/" locale={locale === 'nl' ? 'en' : 'nl'}>
+          <Link
+            href="/"
+            locale={alternativeLocale}
+            onClick={handleSwitchLanguage}
+          >
             {config?.languageToggle}
           </Link>
         </HeaderContent>
