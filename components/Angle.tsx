3c4ae16916b56d79ca34a87874a21195b31b283c
Martijn Hols <git@martijnhols.nl>
2023-02-02 20:30:21 +0100

    Clean up Angle: simplify colors with a linear-gradient & move contact button clip-path to a subelem to reduce rendering issues

--- components/Angle.tsx
+++ components/Angle.tsx
@@ -3,52 +3,46 @@ import styled from '@emotion/styled'
 
 import ContactButtonGlobalHover from './ContactButtonGlobalHover'
 
-// This fixes a rendering bug in Chrome where an invisible line appears when the content is scaled
-export const clipPathBorderFix = css`
-  margin-bottom: -0.5px;
-  margin-top: -0.5px;
-`
-
-const Container = styled.div`
-  position: relative;
-`
-const TopAngle = styled('div', {
+const Container = styled('div', {
   shouldForwardProp: (prop) => prop !== 'inverted',
 })<{ inverted?: boolean }>(
   ({ theme, inverted }) => css`
-    position: absolute;
-    width: 100%;
+    position: relative;
     height: calc(10px + 100vw / 2000 * 30);
-    clip-path: polygon(0 0, 100% 0, 0 100%);
-    background: ${inverted ? 'transparent' : theme.colors.black};
-    ${clipPathBorderFix}
+    background: linear-gradient(
+      to bottom right,
+      /* We need some margin to prevent a jagged edge */
+        ${inverted ? 'transparent' : theme.colors.black} 49.5%,
+      ${inverted ? theme.colors.black : 'transparent'} 50.5%
+    );
   `,
 )
-const BottomAngle = styled('div', {
-  shouldForwardProp: (prop) => prop !== 'inverted',
-})<{ inverted?: boolean }>(
-  ({ theme, inverted }) => css`
-    height: calc(10px + 100vw / 2000 * 30);
-    clip-path: polygon(100% 0, 100% 100%, 0 100%);
-    background: ${inverted ? theme.colors.black : 'transparent'};
-    ${clipPathBorderFix}
-  `,
-)
+const TopAngle = styled.div`
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  width: 100%;
+  height: calc(10px + 100vw / 2000 * 30);
+  clip-path: polygon(0 0, 100% 0, 0 100%);
+`
+const BottomAngle = styled(TopAngle)`
+  clip-path: polygon(100% 0, 100% 100%, 0 100%);
+`
 
 interface Props {
   inverted?: boolean
 }
 
 const Angle = ({ inverted, ...others }: Props) => (
-  <Container {...others}>
-    <TopAngle inverted={inverted}>
+  <Container inverted={inverted} {...others}>
+    <TopAngle>
       <ContactButtonGlobalHover
         inverted={!inverted}
         aria-hidden
         tabIndex={-1}
       />
     </TopAngle>
-    <BottomAngle inverted={inverted}>
+    <BottomAngle>
       <ContactButtonGlobalHover inverted={inverted} aria-hidden tabIndex={-1} />
     </BottomAngle>
   </Container>
