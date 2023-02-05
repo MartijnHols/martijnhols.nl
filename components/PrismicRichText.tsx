b660c115a3b12ae629dcf63aaad5821425c60b4b
Martijn Hols <git@martijnhols.nl>
2023-02-05 11:28:31 +0100

    Use next/image for images inserted in Prismic Rich Text fields

--- components/PrismicRichText.tsx
+++ components/PrismicRichText.tsx
@@ -1,5 +1,7 @@
 import { PrismicRichText as OriginalPrismicRichText } from '@prismicio/react'
 import { RichTextMapSerializerFunction } from '@prismicio/richtext'
+import { RTImageNode } from '@prismicio/types'
+import Image from 'next/image'
 import { ComponentProps, Fragment, ReactElement } from 'react'
 
 interface Props extends ComponentProps<typeof OriginalPrismicRichText> {
@@ -9,6 +11,19 @@ interface Props extends ComponentProps<typeof OriginalPrismicRichText> {
 export const fragmentSerializer: RichTextMapSerializerFunction<
   ReactElement
 > = ({ children, key }) => <Fragment key={key}>{children}</Fragment>
+
+const imageSerializer: RichTextMapSerializerFunction<
+  ReactElement,
+  RTImageNode
+> = ({ key, node }) => (
+  <Image
+    key={key}
+    src={node.url}
+    width={node.dimensions.width}
+    height={node.dimensions.height}
+    alt={node.alt || ''}
+  />
+)
 
 /**
  * Renders PrismicRichText. Removes the wrapper from a non-multiline
@@ -20,14 +35,11 @@ const PrismicRichText = ({
   ...others
 }: Props) => (
   <OriginalPrismicRichText
-    components={
-      !multiline
-        ? {
-            paragraph: fragmentSerializer,
-            ...components,
-          }
-        : components
-    }
+    components={{
+      paragraph: !multiline ? fragmentSerializer : undefined,
+      image: imageSerializer,
+      ...components,
+    }}
     {...others}
   />
 )
