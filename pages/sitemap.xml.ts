cba903ef51307cf4e038101c4afb54d8a910c4f4
Martijn Hols <git@martijnhols.nl>
2023-03-01 12:53:19 +0100

    Add sitemap

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,54 @@
+import { GetServerSideProps } from 'next'
+
+import absoluteUrl from '../utils/absoluteUrl'
+import { createClient, getPages } from '../utils/prismic'
+import prismicLinkResolver from '../utils/prismicLinkResolver'
+import { isFileDownloadPage } from './[slug]'
+
+interface SiteMapUrl {
+  loc: string
+  lastmod?: string
+}
+
+const createUrlXml = (url: SiteMapUrl) => `
+  <url>
+    <loc>${url.loc}</loc>
+    ${url.lastmod ? `<lastmod>${url.lastmod}</lastmod>` : ''}
+  </url>
+`
+const createSiteMapXml = (urls: SiteMapUrl[]) =>
+  `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.map((url) => createUrlXml(url)).join('')}
+  </urlset>
+`.trim()
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const client = createClient()
+  const pages = await getPages(client)
+
+  const sitemap = createSiteMapXml(
+    pages.filter(isFileDownloadPage).map((page) => ({
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      loc: absoluteUrl(prismicLinkResolver(page)),
+      // TODO: lastmod
+    })),
+  )
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=600, stale-while-revalidate=600',
+  )
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+const SiteMap = () => null
+
+export default SiteMap
