1f6c1c2fa4db32e02d2ce7dc34840686494412cb
Martijn Hols <git@martijnhols.nl>
2023-03-08 16:59:32 +0100

    Fix canonicals and alternates trailing slash

--- utils/absoluteUrl.ts
+++ utils/absoluteUrl.ts
@@ -1,4 +1,6 @@
 const absoluteUrl = (path: string) =>
-  `${process.env.NEXT_PUBLIC_PRIMARY_HOST}${path}`
+  `${process.env.NEXT_PUBLIC_PRIMARY_HOST}${
+    path.endsWith('/') ? path.substring(0, path.length - 1) : path
+  }`
 
 export default absoluteUrl
