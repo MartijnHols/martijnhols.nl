00550d2ba46cd397ca420137e8341c3e278ac55e
Martijn Hols <git@martijnhols.nl>
2023-03-12 14:10:41 +0100

    Convert headings media queries to mobile first
    Desktop first was a failed experiment

--- theme/headings.ts
+++ theme/headings.ts
@@ -3,55 +3,55 @@ import { css } from '@emotion/react'
 import { breakpoints, spacing } from '.'
 
 export const h1 = css`
-  font-size: 120px;
+  font-size: 60px;
 
-  @media (max-width: ${breakpoints.DESKTOP_MAX}px) {
-    font-size: 100px;
-  }
-  @media (max-width: ${breakpoints.TABLET_MAX}px) {
+  @media (min-width: ${breakpoints.TABLET}px) {
     font-size: 80px;
   }
-  @media (max-width: ${breakpoints.MOBILE_MAX}px) {
-    font-size: 60px;
+  @media (min-width: ${breakpoints.DESKTOP}px) {
+    font-size: 100px;
+  }
+  @media (min-width: ${breakpoints.DESKTOP_LARGE}px) {
+    font-size: 120px;
   }
 `
 export const h2 = css`
-  font-size: 54px;
+  font-size: 45px;
   line-height: 1.3;
   font-weight: 800;
 
-  @media (max-width: ${breakpoints.MOBILE_MAX}px) {
-    font-size: 45px;
+  @media (min-width: ${breakpoints.TABLET}px) {
+    font-size: 54px;
   }
 `
 export const h3 = css`
-  font-size: 40px;
+  font-size: 30px;
 
-  @media (max-width: ${breakpoints.TABLET_MAX}px) {
+  @media (min-width: ${breakpoints.TABLET}px) {
     font-size: 36px;
   }
-  @media (max-width: ${breakpoints.MOBILE_MAX}px) {
-    font-size: 30px;
+  @media (min-width: ${breakpoints.DESKTOP}px) {
+    font-size: 40px;
   }
 `
 export const h4 = css`
-  font-size: 30px;
+  font-size: 20px;
 
-  @media (max-width: ${breakpoints.TABLET_MAX}px) {
+  @media (min-width: ${breakpoints.TABLET}px) {
     font-size: 22px;
   }
-  @media (max-width: ${breakpoints.MOBILE_MAX}px) {
-    font-size: 20px;
+  @media (min-width: ${breakpoints.DESKTOP}px) {
+    font-size: 30px;
   }
 `
 export const h5 = css`
-  font-size: 24px;
+  font-size: 18px;
 
-  @media (max-width: ${breakpoints.TABLET_MAX}px) {
+  @media (min-width: ${breakpoints.TABLET}px) {
     font-size: 20px;
   }
-  @media (max-width: ${breakpoints.MOBILE_MAX}px) {
-    font-size: 18px;
+  @media (min-width: ${breakpoints.DESKTOP}px) {
+    font-size: 24px;
   }
 `
 
