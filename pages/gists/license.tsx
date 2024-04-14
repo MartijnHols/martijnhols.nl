c7bea1e544937317faac07c2f64b0431d614a29e
Martijn Hols <git@martijnhols.nl>
2024-04-14 10:54:42 +0200

    Slightly rework license gist

--- pages/gists/license.tsx
+++ pages/gists/license.tsx
@@ -10,6 +10,7 @@ export const meta: GistMeta = {
   description:
     'The license to content in my gists, on the rest of my website and the source code of it all.',
   publishedAt: '2024-04-01',
+  updatedAt: '2024-04-14',
   tags: ['meta'],
 }
 
@@ -21,7 +22,8 @@ const LicenseGist = () => (
     </p>
     <p>
       The following license is for the entire website and source code of{' '}
-      <Link href="/">Martijn Hols, a Freelance Senior React Developer</Link>;
+      <Link href="/">Martijn Hols, a Freelance Senior React Developer</Link> on{' '}
+      <Link href="https://github.com/MartijnHols/martijnhols.nl/">GitHub</Link>;
     </p>
 
     <p>You can:</p>
@@ -31,25 +33,34 @@ const LicenseGist = () => (
         ✅ Look through the code, learn and copy parts of it so long as it
         doesn't lead to a similar copy of the site
       </li>
-      <li>✅ Fork it to fix or improve parts</li>
+      <li>✅ Fork it to fix or improve parts of my website</li>
       <li>
-        ✅ Copy any textual content from blog articles, under the CC BY-SA 4.0
-        license (same license as content on StackOverflow)
+        ✅ Copy any content from blog articles, under the{' '}
+        <Link href="https://creativecommons.org/licenses/by-sa/4.0/">
+          CC BY-SA 4.0
+        </Link>{' '}
+        license (same license as content on{' '}
+        <Link href="https://stackoverflow.com/help/licensing">
+          Stack Overflow
+        </Link>
+        )
       </li>
     </ul>
     <p>You can't:</p>
     <ul>
-      <li>❌ Fork or otherwise copy it for (personal) use</li>
       <li>
-        ❌ Copy the layout / styling, including but not limited to the color
+        ❌ Fork or otherwise copy (large parts of) the website for (personal)
+        use
+      </li>
+      <li>
+        ❌ Copy the layout / design, including but not limited to the color
         scheme, typography, layout (including the angles), animations
       </li>
       <li>❌ Copy content outside of blog articles</li>
-      <li>❌ Copy original images</li>
     </ul>
     <p>
       Exceptions or explicit approval may be given via email at{' '}
-      <a href="mailto:website@martijnhols.nl">website@martijnhols.nl</a>.
+      <Link href="mailto:website@martijnhols.nl">website@martijnhols.nl</Link>.
     </p>
   </Gist>
 )
