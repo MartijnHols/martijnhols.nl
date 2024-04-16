7d2e84755da6e14169422fafcc77691674142860
Martijn Hols <git@martijnhols.nl>
2024-04-16 20:08:22 +0200

    Fix sitemap lastmod date for homepage to include project changes

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next'
 
 import absoluteUrl from '../utils/absoluteUrl'
-import { createClient, getPages } from '../utils/prismic'
+import { createClient, getPages, getProjects } from '../utils/prismic'
 import prismicLinkResolver, {
   HOMEPAGE_SLUG,
 } from '../utils/prismicLinkResolver'
@@ -40,21 +40,33 @@ const createSiteMapXml = (urls: SiteMapUrl[]) =>
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const client = createClient()
   const pages = await getPages(client)
+  const projects = await getProjects(client, '*')
 
   const gists = await Promise.all(gistsPromise)
 
   const sitemap = createSiteMapXml([
     ...pages
       .filter((page) => page.data.sitemap !== false)
-      .map((page) => ({
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        loc: absoluteUrl(prismicLinkResolver(page)),
+      .map((page) => {
+        const isHomepage = page.uid === HOMEPAGE_SLUG
+
         // Only include the date part so its format is equal to that of the gists
-        lastmod: page.last_publication_date.split('T')[0],
-        changefreq:
-          page.uid === HOMEPAGE_SLUG ? ('weekly' as const) : undefined,
-        priority: page.uid === HOMEPAGE_SLUG ? 1 : undefined,
-      })),
+        let lastmod = page.last_publication_date.split('T')[0]
+        if (isHomepage) {
+          lastmod = projects.reduce((latest, project) => {
+            const updatedAt = project.last_publication_date.split('T')[0]
+            return updatedAt > latest ? updatedAt : latest
+          }, lastmod)
+        }
+
+        return {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          loc: absoluteUrl(prismicLinkResolver(page)),
+          lastmod,
+          changefreq: isHomepage ? ('weekly' as const) : undefined,
+          priority: isHomepage ? 1 : undefined,
+        }
+      }),
     {
       loc: absoluteUrl('/gists'),
       lastmod: gists.reduce((latest, gist) => {
