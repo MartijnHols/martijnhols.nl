c9e9e8cb466ae4fed5b4fb4032e956b7031242fa
Martijn Hols <git@martijnhols.nl>
2024-04-28 06:51:39 +0200

    Add angles to CodeSnippet to match theme

--- components/CodeSnippet.tsx
+++ components/CodeSnippet.tsx
@@ -3,13 +3,66 @@ import styled from '@emotion/styled'
 import { Highlight, themes } from 'prism-react-renderer'
 // Alternative: https://github.com/react-simple-code-editor/react-simple-code-editor
 
+const Code = styled.code<{ variant?: 'small' | 'regular' }>(
+  ({ theme, variant = 'regular' }) => [
+    css`
+      display: block;
+      // Top and bottom margins are not equal since the angle changes the visual
+      // margin. I believe the left-most column is most important to appear
+      // visually aligned.
+      margin-top: ${theme.spacing.x3}px;
+      margin-bottom: ${theme.spacing.x4}px;
+      // Offset the padding so the code text aligns with the rest of the text
+      margin-left: -1em;
+      margin-right: -1em;
+      padding: ${theme.spacing.x2}px 1em;
+      background: ${theme.colors.black};
+      position: relative;
+
+      ::before,
+      ::after {
+        content: '';
+        position: absolute;
+        display: block;
+        --size: ${theme.spacing.x2}px;
+        height: var(--size);
+        inset: calc(var(--size) * -1) 0;
+      }
+
+      ::before {
+        bottom: auto;
+        background: linear-gradient(
+          to bottom right,
+          /* We need some margin to prevent a jagged edge */ transparent 49.5%,
+          ${theme.colors.black} 50.5%
+        );
+      }
+      ::after {
+        top: auto;
+        background: linear-gradient(
+          to bottom right,
+          /* We need some margin to prevent a jagged edge */
+            ${theme.colors.black} 49.5%,
+          transparent 50.5%
+        );
+      }
+    `,
+    variant === 'small' &&
+      css`
+        padding-top: 9px;
+        padding-bottom: 9px;
+
+        ::before,
+        ::after {
+          --size: ${theme.spacing.x1}px;
+        }
+      `,
+  ],
+)
 const PreformattedContainer = styled.pre(
   ({ theme }) => css`
-    padding: 1em;
-    border-radius: 0.3em;
+    margin: 0;
     scrollbar-color: ${theme.colors.yellow} ${theme.colors.black};
-    // Offset the padding so the code text aligns with the rest of the text
-    margin: 0 -1em;
   `,
 )
 
@@ -25,13 +78,18 @@ export type PrismLanguages =
 interface Props {
   children: string
   language?: PrismLanguages
+  variant?: 'small' | 'regular'
 }
 
-const CodeSnippet = ({ children, language = 'tsx' }: Props) => {
+const CodeSnippet = ({
+  children,
+  language = 'tsx',
+  variant = 'regular',
+}: Props) => {
   const theme = useTheme()
 
   return (
-    <code translate="no">
+    <Code translate="no" variant={variant}>
       <Highlight
         theme={{
           ...themes.oneDark,
@@ -56,7 +114,7 @@ const CodeSnippet = ({ children, language = 'tsx' }: Props) => {
           </PreformattedContainer>
         )}
       </Highlight>
-    </code>
+    </Code>
   )
 }
 
