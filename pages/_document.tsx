63fcaf0bd348f53d1590466e9c579dee51762168
Martijn Hols <git@martijnhols.nl>
2024-05-02 20:36:49 +0200

    Add plausible analytics script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 export default function Document(props: DocumentProps) {
   return (
     <Html lang={props.dangerousAsPath.startsWith('/gists') ? 'en' : 'nl'}>
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="martijnhols.nl"
+          src="https://plausible.io/js/script.js"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
