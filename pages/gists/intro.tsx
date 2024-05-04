4697b9ebab91261e2b52d9a60eaeb55c2ec4c489
Martijn Hols <git@martijnhols.nl>
2024-05-04 12:00:55 +0200

    Update intro gist

--- pages/gists/intro.tsx
+++ pages/gists/intro.tsx
@@ -7,7 +7,7 @@ export const meta: GistMeta = {
   description:
     'The gist of this, why this format, what motivates me, and what to expect.',
   publishedAt: '2024-04-01',
-  updatedAt: '2024-04-21',
+  updatedAt: '2024-05-04',
   tags: [GistTag.Meta],
 }
 
@@ -17,22 +17,22 @@ const Intro = () => (
       On April 1st of all days, I hereby launch my new gists section. This will
       be brief articles, snippets, opinions and answers to questions. Mostly
       about React and closely related things, occasionally some other
-      technologies like Node.js. Maybe I'll throw in some gaming gists as well
-      at some point.
+      technologies like Node.js. Maybe at some point I'll throw in some gists
+      about my hobbies as well.
     </p>
     <h2>Why gists?</h2>
     <p>
-      I have tried writing articles for a very long time, but the scope of them
-      is preventing me from ever finishing them. Much like I do all the time in
-      software development, I realized I had to break down the work into smaller
-      deliverables. Gists are that. I've been writing gists forever, like when I
-      respond to discussions on Reddit or issues on GitHub. Writing those never
-      took me anywhere nearly as long as articles did.
+      I have tried writing long-form articles for a very long time, but the
+      scope of them is preventing me from ever finishing them. Much like I do
+      all the time in software development, I realized I had to break down the
+      work into smaller deliverables. Gists are that. I've been writing gists
+      forever, like when I respond to discussions on Reddit or issues on GitHub.
+      Writing those never took me anywhere nearly as long as articles did.
     </p>
     <p>
-      Most worthwhile topics already have lengthy articles dedicated to them. I
-      don't think I can do better. Instead, I can try to give you the gist of
-      it, maybe my notes on it, and link to these excellent articles.
+      Most worthwhile topics already have lengthy articles dedicated to them.
+      There's no point repeating them. Instead, I can try to give you the gist
+      of my take, and link to these excellent articles as a sort of drilldown.
     </p>
     <p>
       I also had the idea that these gists could end up becoming sort of like my
@@ -42,7 +42,7 @@ const Intro = () => (
       (or tangents).
     </p>
     <p>
-      The first gist I wrote is about{' '}
+      The first gist I wrote was about{' '}
       <a href="./react-switch-statement-rendering">
         using switch statements when rendering in React
       </a>{' '}
@@ -50,12 +50,14 @@ const Intro = () => (
       <a href="https://stackoverflow.com/a/78251113/684353">
         The components are getting unmounted and mounted again if we use switch
         case, Why?
-      </a>{' '}
-      (update: that gist is now unlisted since this deviates far from what I now
-      envision for this section). I'm sure more people new to React wonder how
-      they can conditionally render JSX. This approach will achieve at least two
-      of my goals:
+      </a>
+      . (Update 1 month later:) I unlisted the gist since I realized I want this
+      blog to be focussed on more advanced subjects and basics would distract
+      and detract from that. I still think discussions on Reddit are a great way
+      to collect things to write about, and I can use comments as a way to test
+      some of my ideas.
     </p>
+    <p>This approach will achieve at least two of my goals:</p>
     <p></p>
     <h2>Why write at all?</h2>
     <p>I have many reasons to want to write;</p>
@@ -86,6 +88,7 @@ const Intro = () => (
           *
         </button>
       </li>
+      <li>To get feedback on my ideas and believes</li>
     </ul>
     <p>
       I'm not sure if it was smart to list the backlinks reason as the first
@@ -97,7 +100,6 @@ const Intro = () => (
     <h2>Some other subjects</h2>
     <p>Some other subjects I want to get the gist down about;</p>
     <ul>
-      <li>The tech behind the gists site</li>
       <li>Global state management in React</li>
       <li>
         Why I believe Tailwind is not the right fit for most React projects
