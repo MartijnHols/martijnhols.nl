604782d393d24a3d0c8d76745e61625ee5fe654b
Martijn Hols <git@martijnhols.nl>
2024-05-22 19:29:09 +0200

    Fix anchor link has no offset from top
    This is just a visual margin, we don't have a sticky header (yet)

--- theme/GlobalStyles.tsx
+++ theme/GlobalStyles.tsx
@@ -7,6 +7,9 @@ const GlobalStyles = () => (
   <Global
     styles={(theme) => [
       css`
+        html {
+          scroll-padding-top: 2rem;
+        }
         body {
           font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
             'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
