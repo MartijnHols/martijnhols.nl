685b37bdc2344e59650edcda42e073453b5debfb
Martijn Hols <git@martijnhols.nl>
2024-12-15 19:15:06 +0100

    Unprismic Footer

--- slices/FooterSlice/index.tsx
+++ slices/FooterSlice/index.tsx
@@ -1,12 +1,8 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
-import { Content } from '@prismicio/client'
-import { asLink, isFilled } from '@prismicio/helpers'
 import ContactButtonClipped from '../../components/ContactButtonClipped'
 import Container from '../../components/Container'
 import Link from '../../components/Link'
-import PrismicRichText from '../../components/PrismicRichText'
-import prismicLinkResolver from '../../utils/prismicLinkResolver'
 
 const Section = styled.footer(
   ({ theme }) => css`
@@ -39,108 +35,74 @@ const SocialRow = styled(Row)``
 const BusinessRow = styled(Row)``
 const Item = styled.div``
 
-interface Props {
-  slice: Content.FooterSliceSlice
-}
+const email = 'website@martijnhols.nl'
+const phone = '0657946114'
+const linkedIn = 'https://www.linkedin.com/in/martijnhols/'
+const gitHub = 'https://github.com/MartijnHols'
+const twitter = 'https://twitter.com/MartijnHols'
+const kvk = '72214473'
+const btw = 'NL002216536B75'
 
-const FooterSlice = ({ slice }: Props) => {
-  const linkedIn = asLink(slice.primary.linkedIn, prismicLinkResolver)
-  const gitHub = asLink(slice.primary.gitHub, prismicLinkResolver)
-  const twitter = asLink(slice.primary.twitter, prismicLinkResolver)
-
-  return (
-    <Section className="inverted" id="footer" role="contentinfo">
-      <Container>
-        <ContactRow>
-          <Label>{slice.primary.contactLabel}</Label>
-          <div>
-            <Values>
-              {slice.primary.email && (
-                <Item>
-                  <a href={`mailto:${slice.primary.email}`}>
-                    {slice.primary.email}
-                  </a>
-                </Item>
-              )}
-              {slice.primary.whatsApp && (
-                <Item>
-                  <a
-                    href={`https://api.whatsapp.com/send?phone=${slice.primary.whatsApp}`}
-                  >
-                    WhatsApp
-                  </a>
-                </Item>
-              )}
-              {slice.primary.phone && (
-                <Item>
-                  <a href={`tel:${slice.primary.phone}`}>
-                    {slice.primary.phone}
-                  </a>
-                </Item>
-              )}
-            </Values>
-            <small>
-              Ik ben het beste bereikbaar op dinsdag, donderdag, vrijdag, en in
-              het weekend.
-            </small>
-          </div>
-        </ContactRow>
-        {isFilled.richText(slice.primary.contactAnnotation) && (
-          <Row>
-            <Label />
-            <Values>
-              <PrismicRichText field={slice.primary.contactAnnotation} />
-            </Values>
-          </Row>
-        )}
-        {slice.primary.city && (
-          <Row>
-            <Label>{slice.primary.locationLabel}</Label>
-            <Values>
-              <Item>{slice.primary.city}</Item>
-            </Values>
-          </Row>
-        )}
-        <SocialRow>
-          <Label>{slice.primary.socialLabel}</Label>
+const FooterSlice = () => (
+  <Section className="inverted" id="footer" role="contentinfo">
+    <Container>
+      <ContactRow>
+        <Label>Contact</Label>
+        <div>
           <Values>
-            {linkedIn && (
-              <Item>
-                <Link href={linkedIn}>LinkedIn</Link>
-              </Item>
-            )}
-            {gitHub && (
-              <Item>
-                <Link href={gitHub}>GitHub</Link>
-              </Item>
-            )}
-            {twitter && (
-              <Item>
-                <Link href={twitter}>Twitter</Link>
-              </Item>
-            )}
+            <Item>
+              <a href={`mailto:${email}`}>{email}</a>
+            </Item>
+            <Item>
+              <a href={`tel:${phone}`}>{phone}</a>
+            </Item>
           </Values>
-        </SocialRow>
-        <BusinessRow>
-          <Label>{slice.primary.companyLabel}</Label>
-          <Values>
-            {slice.primary.kvk && (
-              <Item>
-                <strong>KVK:</strong> {slice.primary.kvk}
-              </Item>
-            )}
-            {slice.primary.btw && (
-              <Item>
-                <strong>BTW:</strong> {slice.primary.btw}
-              </Item>
-            )}
-          </Values>
-        </BusinessRow>
-      </Container>
+          <small>
+            Ik ben het beste bereikbaar op dinsdag, donderdag, vrijdag, en in
+            het weekend.
+          </small>
+        </div>
+      </ContactRow>
+      <Row>
+        <Label>Locatie</Label>
+        <Values>
+          <Item>
+            <strong>Barneveld, Gelderland</strong>. Beschikbaar voor opdrachten
+            binnen 45 minuten, zoals Amersfoort, Veenendaal, Ede, Wageningen,
+            Apeldoorn, Utrecht, Zeist, Hilversum, Rhenen, Nijkerk, Leusden, en
+            omstreken
+          </Item>
+        </Values>
+      </Row>
+      <SocialRow>
+        <Label>Social</Label>
+        <Values>
+          <Item>
+            <Link href={linkedIn}>LinkedIn</Link>
+          </Item>
+          <Item>
+            <Link href={gitHub}>GitHub</Link>
+          </Item>
+          <Item>
+            <Link href={twitter}>Twitter</Link>
+          </Item>
+        </Values>
+      </SocialRow>
+      <BusinessRow>
+        <Label>Bedrijf</Label>
+        <Values>
+          <Item>
+            <strong>KVK:</strong> {kvk}
+          </Item>
+          <Item>
+            <strong>BTW:</strong> {btw}
+          </Item>
+        </Values>
+      </BusinessRow>
+    </Container>
 
-      <ContactButtonClipped inverted />
-    </Section>
-  )
-}
+    <ContactButtonClipped inverted />
+  </Section>
+)
 
 export default FooterSlice
