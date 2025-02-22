4ad2b9a3d006ac65ffe0c025fba252686eeb3cd2
Martijn Hols <git@martijnhols.nl>
2025-02-22 16:24:13 +0100

    Add static export config
    This is only intended for testing the performance benefits, I don't intend to use this in production.

--- next.config.js
+++ next.config.js
@@ -18,6 +18,8 @@ if (!process.env.PAGE_REVALIDATE_INTERVAL) {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
+  output: 'export',
+  images: { unoptimized: true },
   reactStrictMode: true,
   // Values that can be different per instance of the server
   serverRuntimeConfig: {
