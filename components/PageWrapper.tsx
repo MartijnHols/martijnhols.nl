9af00cc5300cc77e137254091e135d71f9f3b8cf
Martijn Hols <git@martijnhols.nl>
2025-03-13 11:55:48 +0100

    Fix bug where scrollbar was invisible on iOS Safari
    The overscroll color fix for Chrome broke iOS Safari. This is the exact same issue: https://frontendmasters.com/blog/how-to-fix-the-invisible-scrollbar-issue-in-ios/

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,10 +1,8 @@
-import { Global, css } from '@emotion/react'
 import styled from '@emotion/styled'
 import { ReactNode } from 'react'
 
 const Container = styled.div`
   min-height: 100vh;
-  background: var(--white);
 `
 
 interface Props {
@@ -12,19 +10,7 @@ interface Props {
 }
 
 const PageWrapper = ({ children, ...others }: Props) => (
-  <>
-    <Container {...others}>{children}</Container>
-    <Global
-      styles={css`
-        body {
-          // The body is made black since the top and bottom bars are black,
-          // this ensures overpulling has the same bg as the bars. The
-          // Container changes it back to white, so it actually looks white.
-          background: var(--black);
-        }
-      `}
-    />
-  </>
+  <Container {...others}>{children}</Container>
 )
 
 export default PageWrapper
